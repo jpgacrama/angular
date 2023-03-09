99cd3ae32692d7eabd23cda88c86b440cd400352
Jonas Gacrama <jonasgacrama@gmail.com>
2023-03-09 17:36:39 +0800

    cc-setters: Added get() and set() vName functions to VcLogsComponent

--- start_here/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ start_here/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./vc-logs.component.scss'],
 })
 export class VcLogsComponent implements OnInit {
-  @Input() vName;
+  _vName: string;
+  @Input() get vName() {
+    return this._vName;
+  }
+  set vName(name: string) {
+    this._vName = name;
+  }
   logs: string[] = [];
   constructor() {}
 
