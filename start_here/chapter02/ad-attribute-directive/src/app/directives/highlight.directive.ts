f308b0c183a039185ebfe57149abc58d2cec6686
Jonas Gacrama <jonasgacrama@gmail.com>
2023-03-11 20:51:50 +0800

    ad-attribute-directive: Highlighting is now OK
    But I need not to use nativeElement Anymore

--- start_here/chapter02/ad-attribute-directive/src/app/directives/highlight.directive.ts
+++ start_here/chapter02/ad-attribute-directive/src/app/directives/highlight.directive.ts
@@ -12,19 +12,23 @@ import {
 export class HighlightDirective implements OnChanges {
   @Input() highlightText = '';
   @Input() highlightColor = 'yellow';
+  originalHTML = '';
   constructor(private el: ElementRef) {}
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.highlightText.firstChange) {
+      this.originalHTML = this.el.nativeElement.innerHTML;
       return;
     }
     const { currentValue } = changes.highlightText;
     if (currentValue) {
       const regExp = new RegExp(`(${currentValue})`, 'gi');
-      this.el.nativeElement.innerHTML = this.el.nativeElement.innerHTML.replace(
+      this.el.nativeElement.innerHTML = this.originalHTML.replace(
         regExp,
-        `<span style="background-color:${this.highlightColor}">\$1</span>`
+        `<span style="background-color: ${this.highlightColor}">\$1</span>`
       );
+    } else {
+      this.el.nativeElement.innerHTML = this.originalHTML;
     }
   }
 }
