19d492fd1d9fa0c98166e62513abb849c872d86d
Jonas Gacrama <jonasgacrama@gmail.com>
2023-03-11 20:36:13 +0800

    ad-attribute-directive: Used nativeElement to render highlighting
    But this is NOT recommended by Angular. They recommend to use Renderer2 instead

--- start_here/chapter02/ad-attribute-directive/src/app/directives/highlight.directive.ts
+++ start_here/chapter02/ad-attribute-directive/src/app/directives/highlight.directive.ts
@@ -1,10 +1,30 @@
-import { Directive, Input } from '@angular/core';
+import {
+  Directive,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  ElementRef,
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]',
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnChanges {
   @Input() highlightText = '';
   @Input() highlightColor = 'yellow';
-  constructor() {}
+  constructor(private el: ElementRef) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.highlightText.firstChange) {
+      return;
+    }
+    const { currentValue } = changes.highlightText;
+    if (currentValue) {
+      const regExp = new RegExp(`(${currentValue})`, 'gi');
+      this.el.nativeElement.innerHTML = this.el.nativeElement.innerHTML.replace(
+        regExp,
+        `<span style="background-color:${this.highlightColor}">\$1</span>`
+      );
+    }
+  }
 }
