41c08f2e1c7981a9b8da437816a24ddc113161e4
Jonas Gacrama <jonasgacrama@gmail.com>
2023-03-11 22:08:57 +0800

    ng-read-time-directive: Created ngOnInit() and createTimeString()

--- start_here/chapter02/ng-read-time-directive/src/app/directives/read-time.directive.ts
+++ start_here/chapter02/ng-read-time-directive/src/app/directives/read-time.directive.ts
@@ -1,8 +1,35 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, Renderer2, ElementRef } from '@angular/core';
+
+export interface ReadTimeConfig {
+  wordsPerMinute: number;
+}
 
 @Directive({
   selector: '[appReadTime]',
 })
 export class ReadTimeDirective {
-  constructor() {}
+  @Input() configuration: ReadTimeConfig = {
+    wordsPerMinute: 200,
+  };
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit() {
+    const text = this.el.nativeElement.textContent;
+    const wordCount = text.trim().split(/\s+/g).length;
+    const readTime = Math.ceil(wordCount / this.configuration.wordsPerMinute);
+
+    const readTimeElement = this.renderer.createText(`${readTime} min read`);
+    this.renderer.appendChild(this.el.nativeElement, readTimeElement);
+  }
+
+  createTimeString(timeInMinutes: number) {
+    if (timeInMinutes === 1) {
+      return '1 minute';
+    } else if (timeInMinutes < 1) {
+      return '< 1 minute';
+    } else {
+      return `${timeInMinutes} minutes`;
+    }
+  }
 }
