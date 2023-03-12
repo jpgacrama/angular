3384fd4bba8551ddccee53c361f59bf3b02dfb7c
Jonas Gacrama <jonasgacrama@gmail.com>
2023-03-12 17:56:54 +0800

    ng-scroll-to-directive: Created my first scroll to directive

--- start_here/chapter02/ng-scroll-to-directive/src/app/directives/scroll-to.directive.ts
+++ start_here/chapter02/ng-scroll-to-directive/src/app/directives/scroll-to.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appScrollTo]'
+  selector: '[appScrollTo]',
 })
 export class ScrollToDirective {
+  @Input() target: string = '';
 
-  constructor() { }
+  constructor() {}
 
+  @HostListener('click')
+  onClick() {
+    const targetElement = document.querySelector(this.target);
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
