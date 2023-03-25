5927db0915d4933cf49a921dae88b3f6424302bf
Jonas Gacrama <jonasgacrama@gmail.com>
2023-03-25 15:23:57 +0800

    ng-value-providers: Employee modules is prevented from deleting. But admin does not have a delete button 🤔

---
+++ start_here/chapter03/ng-value-providers/src/app/employee/constants/employee-config.ts
@@ -0,0 +1,4 @@
+import { IAppConfig } from 'src/app/constants/app-config';
+export const EmployeeConfig: IAppConfig = {
+  canDeleteItems: false,
+};
--- start_here/chapter03/ng-value-providers/src/app/employee/employee.module.ts
+++ start_here/chapter03/ng-value-providers/src/app/employee/employee.module.ts
@@ -3,14 +3,17 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { EmployeeComponent } from './employee.component';
-
+import { APP_CONFIG } from '../constants/app-config';
+import { EmployeeConfig } from './constants/employee-config';
 
 @NgModule({
   declarations: [EmployeeComponent],
-  imports: [
-    CommonModule,
-    EmployeeRoutingModule,
-    SharedModule
-  ]
+  imports: [CommonModule, EmployeeRoutingModule, SharedModule],
+  providers: [
+    {
+      provide: APP_CONFIG,
+      useValue: EmployeeConfig,
+    },
+  ],
 })
-export class EmployeeModule { }
+export class EmployeeModule {}
