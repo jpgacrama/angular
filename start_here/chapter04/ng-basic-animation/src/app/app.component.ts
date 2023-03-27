99849f47abc9238ffcee307bb6345af0db401555
Jonas Gacrama <jonasgacrama@gmail.com>
2023-03-27 20:34:36 +0800

    ng-basic-animation: Added the btn-active-text and btn-inactive-text states

--- start_here/chapter04/ng-basic-animation/src/app/app.component.ts
+++ start_here/chapter04/ng-basic-animation/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 import { SocialCardType } from './constants/social-card-type';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('socialBtnText', [
+      state(
+        'btn-active-text',
+        style({
+          width: '80px',
+          visibility: 'visible',
+        })
+      ),
+      state(
+        'btn-inactive-text',
+        style({
+          width: '0px',
+          visibility: 'hidden',
+        })
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'ng-dynamic-components';
