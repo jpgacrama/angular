3b1a08f9401ed2c400e80107c99a8f32c98a6162
Jonas Gacrama <jonasgacrama@gmail.com>
2023-04-24 18:14:08 +0800

    animations-using-keyframes: Committing changes before putting my own touch

--- start_here/chapter04/animations-using-keyframes/src/app/animations.ts
+++ start_here/chapter04/animations-using-keyframes/src/app/animations.ts
@@ -4,21 +4,34 @@ import {
   style,
   animate,
   transition,
+  keyframes,
 } from '@angular/animations';
 
 export const cardAnimation = trigger('cardAnimation', [
-  state('active', style({
-    color: 'rgb(51, 51, 51)',
-    backgroundColor: 'white'
-  })),
+  state(
+    'active',
+    style({
+      color: 'rgb(51, 51, 51)',
+      backgroundColor: 'white',
+    })
+  ),
   transition('void => *', [
     style({
       transform: 'translateX(-200px)',
-      opacity: 0
+      opacity: 0,
     }),
-    animate('0.2s ease', style({
-      transform: 'translateX(0)',
-      opacity: 1
-    }))
+    animate(
+      '1.5s ease',
+      keyframes([
+        style({
+          transform: 'translateX(-200px) scale3d(0.4, 0.4, 0.4)',
+          offset: 0,
+        }),
+        style({
+          transform: 'translateX(0)',
+          offset: 1,
+        }),
+      ])
+    ),
   ]),
-])
+]);
