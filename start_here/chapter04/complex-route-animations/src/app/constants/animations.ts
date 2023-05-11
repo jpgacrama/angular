11aaeaea534bfc2993fb3f5683fd502afeb2d45d
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-11 18:34:39 +0800

    complex-route-animations: 3D animation is now available

--- start_here/chapter04/complex-route-animations/src/app/constants/animations.ts
+++ start_here/chapter04/complex-route-animations/src/app/constants/animations.ts
@@ -41,12 +41,37 @@ export const ROUTE_ANIMATION = trigger('routeAnimation', [
         [
           animate(
             '1s ease-in',
-            style({
-              opacity: 0,
-            })
+            keyframes([
+              style({
+                opacity: 1,
+                offset: 0,
+                transform: 'rotateY(0) translateX(0) translateZ(0)',
+              }),
+              style({
+                offset: 0.25,
+                transform:
+                  'rotateY(45deg) translateX(25%) translateZ(100px) translateY(5%)',
+              }),
+              style({
+                offset: 0.5,
+                transform:
+                  'rotateY(90deg) translateX(75%) translateZ(400px) translateY(10%)',
+              }),
+              style({
+                offset: 0.75,
+                transform:
+                  'rotateY(135deg) translateX(75%) translateZ(800px) translateY(15%)',
+              }),
+              style({
+                opacity: 0,
+                offset: 1,
+                transform:
+                  'rotateY(180deg) translateX(0) translateZ(1200px) translateY(25%)',
+              }),
+            ])
           ),
         ],
-        { optional: true }
+        optional
       ),
       query(
         ':enter',
