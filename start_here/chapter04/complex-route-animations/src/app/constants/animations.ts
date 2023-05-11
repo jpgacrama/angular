d7a382241309aa6744d5b9672f7aa98c316446f1
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-11 10:44:20 +0800

    complex-route-animations: Added 3D-like animations when entering

--- start_here/chapter04/complex-route-animations/src/app/constants/animations.ts
+++ start_here/chapter04/complex-route-animations/src/app/constants/animations.ts
@@ -5,6 +5,7 @@ import {
   query,
   animate,
   group,
+  keyframes,
 } from '@angular/animations';
 
 const optional = { optional: true };
@@ -13,6 +14,7 @@ export const ROUTE_ANIMATION = trigger('routeAnimation', [
   transition('* <=> *', [
     style({
       position: 'relative',
+      perspective: '1000px',
     }),
     query(
       ':enter, :leave',
@@ -51,9 +53,31 @@ export const ROUTE_ANIMATION = trigger('routeAnimation', [
         [
           animate(
             '1s ease-out',
-            style({
-              opacity: 1,
-            })
+            keyframes([
+              style({
+                opacity: 0,
+                offset: 0,
+                transform: 'rotateY(180deg) translateX(25%) translateZ(1200px)',
+              }),
+              style({
+                offset: 0.25,
+                transform:
+                  'rotateY(225deg) translateX(-25%) translateZ(1200px)',
+              }),
+              style({
+                offset: 0.5,
+                transform: 'rotateY(270deg) translateX(-50%) translateZ(400px)',
+              }),
+              style({
+                offset: 0.75,
+                transform: 'rotateY(315deg) translateX(-50%) translateZ(25px)',
+              }),
+              style({
+                opacity: 1,
+                offset: 1,
+                transform: 'rotateY(360deg) translateX(0) translateZ(0)',
+              }),
+            ])
           ),
         ],
         { optional: true }
