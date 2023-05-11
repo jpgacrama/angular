98b57af2827331451bd5e444531a32a3dd0e0163
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-11 20:00:55 +0800

    rxjs-operators-instance-methods: Fixed problems and added Null checking

--- start_here/chapter05/rxjs-operators-instance-methods/src/app/app.component.ts
+++ start_here/chapter05/rxjs-operators-instance-methods/src/app/app.component.ts
@@ -4,27 +4,28 @@ import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  subscription: Subscription = null;
+  subscription: Subscription | null;
   inputStreamData = ['john wick', 'inception', 'interstellar'];
-  outputStreamData = [];
+  outputStreamData: number[] = [];
 
   ngOnInit() {
+    this.subscription = new Subscription();
   }
 
   startStream() {
     const streamSource = interval(1500);
-    this.subscription = streamSource.subscribe(input => {
+    this.subscription = streamSource.subscribe((input) => {
       this.outputStreamData.push(input);
     });
   }
 
   stopStream() {
-    this.subscription.unsubscribe();
-    this.subscription = null;
+    if (this.subscription !== null) {
+      this.subscription.unsubscribe();
+      this.subscription = null;
+    }
   }
-
-
 }
