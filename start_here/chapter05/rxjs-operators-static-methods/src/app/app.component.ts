82e88046fd4f6f802fd2ed51301f44bc163504a8
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-13 11:36:47 +0800

    rxjs-operators-static-methods: Merged two streams but extracted data from each of them for simultaneous processing

--- start_here/chapter05/rxjs-operators-static-methods/src/app/app.component.ts
+++ start_here/chapter05/rxjs-operators-static-methods/src/app/app.component.ts
@@ -1,54 +1,75 @@
 import { Component } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, partition, mergeWith, tap } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   subscription: Subscription = null;
-  combinedStreamData = [{
-    type: 'movie',
-    title: 'john wick'
-  }, {
-    type: 'cartoon',
-    title: 'Thunder Cats'
-  }, {
-    type: 'movie',
-    title: 'inception'
-  }, {
-    type: 'cartoon',
-    title: 'Dragon Ball Z'
-  }, {
-    type: 'cartoon',
-    title: 'Ninja Turtles'
-  }, {
-    type: 'movie',
-    title: 'interstellar'
-  }];
+  movies = [];
+  cartoons = [];
+  combinedStreamData = [
+    {
+      type: 'movie',
+      title: 'john wick',
+    },
+    {
+      type: 'cartoon',
+      title: 'Thunder Cats',
+    },
+    {
+      type: 'movie',
+      title: 'inception',
+    },
+    {
+      type: 'cartoon',
+      title: 'Dragon Ball Z',
+    },
+    {
+      type: 'cartoon',
+      title: 'Ninja Turtles',
+    },
+    {
+      type: 'movie',
+      title: 'interstellar',
+    },
+  ];
   outputStreamData = [];
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   startStream() {
     const streamSource = interval(1500).pipe(
-      map(input => {
+      map((input) => {
         const index = input % this.combinedStreamData.length;
         return this.combinedStreamData[index];
       })
     );
-    this.subscription = streamSource
-      .subscribe(input => {
+    const [moviesStream, cartoonsStream] = partition(
+      streamSource,
+      (item) => item.type === 'movie'
+    );
+    this.subscription = moviesStream
+      .pipe(
+        tap((movie) => {
+          this.movies.push(movie.title);
+        }),
+        mergeWith(
+          cartoonsStream.pipe(
+            tap((cartoon) => {
+              this.cartoons.push(cartoon.title);
+            })
+          )
+        )
+      )
+      .subscribe((input) => {
         this.outputStreamData.push(input);
       });
-
   }
-
   stopStream() {
     this.subscription.unsubscribe();
     this.subscription = null;
   }
-
 }
