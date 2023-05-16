81fe43a9c1973d01c72458393e342748e7ece9a8
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-16 16:01:32 +0800

    using-async-pipe: Now used async pipe to automatically subscribe and unsubscribe

--- start_here/chapter05/using-async-pipe/src/app/app-routing.module.ts
+++ start_here/chapter05/using-async-pipe/src/app/app-routing.module.ts
@@ -3,20 +3,24 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{
-  path: '',
-  redirectTo: '/home',
-  pathMatch: 'full'
-}, {
-  path: 'home',
-  component: HomeComponent
-}, {
-  path: 'about',
-  component: AboutComponent
-}];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {})],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
