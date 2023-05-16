c7ad4a8ec8731e9459bfe949244cbded5e726bfe
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-16 16:26:56 +0800

    using-combinelatest-operator: Initial commit

--- start_here/chapter05/using-combinelatest-operator/src/app/home/home.component.ts
+++ start_here/chapter05/using-combinelatest-operator/src/app/home/home.component.ts
@@ -10,30 +10,32 @@ import { COLORS } from '../constants';
 export class HomeComponent implements OnInit, OnDestroy {
   sizeOptions = [100, 200, 300, 400, 500, 600, 700];
   colorOptions = COLORS;
-  borderRadiusOptions = [4, 6, 8, 10, 12, 14, 16, 18, 20]
+  borderRadiusOptions = [4, 6, 8, 10, 12, 14, 16, 18, 20];
 
   boxStyles: {
-    width: string,
-    height: string,
-    backgroundColor: string,
-    color: string
-    borderRadius: string
-  }
+    width: string;
+    height: string;
+    backgroundColor: string;
+    color: string;
+    borderRadius: string;
+  };
 
   boxForm = new FormGroup({
     size: new FormControl(''),
     borderRadius: new FormControl(''),
     textColor: new FormControl(''),
     backgroundColor: new FormControl(''),
-  })
+  });
 
   constructor() {}
 
   ngOnInit() {
-    this.boxForm.get('size').setValue(this.sizeOptions[0]);
+    this.boxForm.get('size').setValue(String(this.sizeOptions[0]));
     this.boxForm.get('backgroundColor').setValue(this.colorOptions[0]);
     this.boxForm.get('textColor').setValue(this.colorOptions[1]);
-    this.boxForm.get('borderRadius').setValue(this.borderRadiusOptions[0]);
+    this.boxForm
+      .get('borderRadius')
+      .setValue(String(this.borderRadiusOptions[0]));
     this.applyChanges();
   }
 
@@ -43,8 +45,8 @@ export class HomeComponent implements OnInit, OnDestroy {
       height: `${size}px`,
       backgroundColor,
       color,
-      borderRadius: `${borderRadius}px`
-    }
+      borderRadius: `${borderRadius}px`,
+    };
   }
 
   applyChanges() {
@@ -52,8 +54,8 @@ export class HomeComponent implements OnInit, OnDestroy {
       this.boxForm.get('size').value,
       this.boxForm.get('backgroundColor').value,
       this.boxForm.get('textColor').value,
-      this.boxForm.get('borderRadius').value,
-    )
+      this.boxForm.get('borderRadius').value
+    );
   }
 
   ngOnDestroy() {}
