204739fda288281fd9a77c3e6c0c1a96ab852729
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-28 19:14:50 +0800

    ngrx-actions-reducer: Initial commit using StoreModule

--- start_here/chapter06/ngrx-actions-reducer/src/app/app.module.ts
+++ start_here/chapter06/ngrx-actions-reducer/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    StoreModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent],
