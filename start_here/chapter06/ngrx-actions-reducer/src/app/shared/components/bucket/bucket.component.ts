25b381a67621a1e5a6afe5a229c7256ee75f5d23
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-29 16:21:31 +0800

    ngrx-actions-reducer: Adding and Removing items from Bucket is now added to Bucket Component

--- start_here/chapter06/ngrx-actions-reducer/src/app/shared/components/bucket/bucket.component.ts
+++ start_here/chapter06/ngrx-actions-reducer/src/app/shared/components/bucket/bucket.component.ts
@@ -4,6 +4,12 @@ import { BucketService } from 'src/app/services/bucket.service';
 import { Fruit } from '../../../constants/fruit';
 import { IFruit } from '../../../interfaces/fruit.interface';
 import { ANIMATIONS } from '../../../constants/animations';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducer';
+import {
+  addItemToBucket,
+  removeItemFromBucket,
+} from 'src/app/store/app.actions';
 @Component({
   selector: 'app-bucket',
   templateUrl: './bucket.component.html',
@@ -14,7 +20,10 @@ export class BucketComponent implements OnInit {
   bucket$: Observable<IFruit[]>;
   selectedFruit: Fruit = '' as null;
   fruits: string[] = Object.values(Fruit);
-  constructor(private bucketService: BucketService) {}
+  constructor(
+    private bucketService: BucketService,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit(): void {
     this.bucket$ = this.bucketService.bucket$;
@@ -27,8 +36,10 @@ export class BucketComponent implements OnInit {
       name: this.selectedFruit,
     };
     this.bucketService.addItem(newItem);
+    this.store.dispatch(addItemToBucket(newItem));
   }
   deleteFromBucket(fruit: IFruit) {
     this.bucketService.removeItem(fruit);
+    this.store.dispatch(removeItemFromBucket(fruit));
   }
 }
