b3a5524ef0e43d7295829bbcfe3e27cd361b61f1
Jonas Gacrama <jonasgacrama@gmail.com>
2023-05-30 10:40:35 +0800

    using-ngrx-selectors: Final commit before moving to a new chapter

--- start_here/chapter06/using-ngrx-effects/src/app/store/app.reducer.ts
+++ start_here/chapter06/using-ngrx-effects/src/app/store/app.reducer.ts
@@ -1,16 +1,21 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { IUser } from '../core/interfaces/user.interface';
+import { getUsersSuccess } from './app.actions';
 
 export interface AppState {
   users: IUser[];
 }
 
 const initialState: AppState = {
-  users: []
-}
+  users: [],
+};
 
 const appReducer = createReducer(
   initialState,
+  on(getUsersSuccess, (state, action) => ({
+    ...state,
+    users: action.users,
+  }))
 );
 
 export function reducer(state: AppState = initialState, action: Action) {
